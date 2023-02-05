68fea0487aee3a1a25a3c41486167261ac730ff8
Jackson <jrmullen2@users.noreply.github.com>
2023-02-05 09:58:55 -0800

    Create playRound function and write implementation of function

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ function getComputerChoice()
     switch(randomChoice)
     {
         case randomNumber >= 0 && randomNumber < 34:
-            randomChoice = "Rock";
+            randomChoice = "rock";
             break;
         
         case randomNumber >= 34 && randomNumber < 67:
-            randomChoice = "Paper";
+            randomChoice = "paper";
             break;
         
         case randomNumber >= 67 && randonmNumber < 101:
-            randomChoice = "Scissors";
+            randomChoice = "scissors";
             break;
         
         default: 
@@ -21,4 +21,40 @@ function getComputerChoice()
             break; 
     }
     return randomChoice;   
+}
+function playRound(playerSelection, computerSelection)
+{
+    let result = "";
+    playerSelection = playerSelection.toLowerCase();
+    switch(playerSelection, computerSelection)
+    {
+        case playerSelection === "rock" && computerSelection === "paper":
+            result = "You Lose! Paper beats Rock";
+            break;
+
+        case playerSelection === "paper" && computerSelection === "scissors":
+            result = "You Lose! Scissors beats Paper";
+            break;
+
+        case playerSelection === "scissors" && computerSelection === "rock":
+                result = "You Lose! Rock beats Scissors";
+                break;
+
+        case playerSelection === "paper" && computerSelection === "rock":
+            result = "You Win! Paper beats Rock";
+            break;
+
+        case playerSelection === "scissors" && computerSelection === "paper":
+            result = "You Win! Scissors beats Paper";
+            break;
+
+        case playerSelection === "rock" && computerSelection === "scissors":
+            result = "You Win! Rock beats Scissors";
+            break;
+
+        default:
+            result = "";
+            break;
+    }
+    return result;  
 }
